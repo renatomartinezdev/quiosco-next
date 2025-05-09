a12a10df36386ab15d56a44d27fdd288190351f8
renatomartinezdev <renatom1912@gmail.com>
2025-05-08 22:54:08 -0300

    firts commit

---
+++ app/products/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function ProductsPage() {
+
+    
+  return (
+    <h1>Product Page</h1>
+  )
+}
---
+++ components/ui/CategoryIcon.tsx
@@ -0,0 +1,7 @@
+
+
+export default function CategoryIcon() {
+  return (
+    <div>CategoryIcon</div>
+  )
+}
